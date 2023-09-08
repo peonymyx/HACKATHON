a5fc67ac7e34efcbc0eae9080b00a1aa25e8d661
peonymyx <peonymyx@gmail.com>
2023-09-08 22:50:53 +0700

    bt

--- scoreboard/src/components/Form.jsx
+++ scoreboard/src/components/Form.jsx
@@ -1,49 +1,64 @@
 import { randomId } from '@mieuteacher/meomeojs';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import TableUsers from './Table';
 
 export default function Form() {
     const [users, setUsers] = useState([]);
     function addUser(e) {
         e.preventDefault();
-        let user={
+        let user = {
             id: randomId(),
             name: e.target.user.value,
             score: 0,
         }
-        setUsers([user,...users]);
-        
-    }
-    useEffect(()=> {
+        setUsers([user, ...users]);
 
-    },[users]);
+    }
+    useEffect(() => {
+
+    }, [users]);
 
     function deletePlayer(userID) {
         setUsers(users.filter(user => user.id != userID))
     }
 
-    function scorePlayer(userID, type){
+    function scorePlayer(userID, type) {
         setUsers(users.map(user => {
             if (user.id == userID) {
-               if(type == '-'){
-                if(user.score == 0){
-                    return user
-                }else{
-                    user.score -=1; 
+                if (type == '-') {
+                    if (user.score == 0) {
+                        return user
+                    } else {
+                        user.score -= 1;
+                    }
+
                 }
-               
-               }
-               if(type == '+'){
-                user.score += 1;
-               }
+                if (type == '+') {
+                    user.score += 1;
+                }
             }
             return user
         }))
-         
+
     }
+      let totalPlayers = useMemo(() => {
+          return users.length
+      },[users])
+    let result = 0;
+    let totalPoints = useMemo(() => {
+        for (let i = 0; i < users.length; i++) {
+            result += users[i].score;
+        }
+        return result
+    }, [users])
     return (
         <div>
-            <TableUsers users={users} setUsers={setUsers} deletePlayer={deletePlayer} scorePlayer={scorePlayer}/>
+            <div id="container">
+                <span id='Nav_players'>Players: {totalPlayers}</span>
+                <span id='Nav_total'>Total Points: {totalPoints} </span>
+            </div>
+
+            <TableUsers users={users} setUsers={setUsers} deletePlayer={deletePlayer} scorePlayer={scorePlayer} />
             <form id='form' onSubmit={(e) => {
                 addUser(e)
             }
--- scoreboard/src/components/Navbar.jsx
+++ scoreboard/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Form from './Form'
 export default function Navbar() {
+  
   return (
     <div>
         <h1 id='title'>Bảng Điểm</h1>
